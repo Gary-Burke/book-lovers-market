917f2c00f7ef3ba224063f0e290aa6172f331ce9
gary-burke <garylewisburke@gmail.com>
2025-12-20 19:16:50 +0100

    Add Book model to library app

--- library/models.py
+++ library/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Owned"), (1, "For Sale"), (2, "Sold"))
 
 # Create your models here.
+
+
+class Book(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="books"
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13)
+    cover_url = models.URLField(blank=True, null=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.isbn})"
