951f75e9db43dc03fcfe01a9bfd9b3d5804d9fa2
gary-burke <garylewisburke@gmail.com>
2025-12-22 11:54:00 +0100

    Add code to ISBNForm to remove all hyphens and spaces from user input

--- library/forms.py
+++ library/forms.py
@@ -1,9 +1,14 @@
 from django import forms
+import re
 
 
 class ISBNForm(forms.Form):
     isbn = forms.CharField(
-        max_length=13,
+        max_length=30,
         label="ISBN",
         widget=forms.TextInput(attrs={"placeholder": "Enter ISBN"})
     )
+
+    def clean_isbn(self):
+        isbn = self.cleaned_data["isbn"]
+        return re.sub(r"[-\s]", "", isbn)
