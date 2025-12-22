f5ae2be3951f5ab4fa9bf0808b13ab66102a04a9
gary-burke <garylewisburke@gmail.com>
2025-12-22 21:23:48 +0100

    Add slug field to prepopulated fields in admin for Model Book

--- library/admin.py
+++ library/admin.py
@@ -10,7 +10,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "isbn", "status")
     search_fields = ["title", "author", "isbn", "status"]
     list_filter = ("title", "author", "isbn", "status")
-    
+    prepopulated_fields = {'slug': ('title',)}
 
 
 # Register your models here.
