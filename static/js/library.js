ea39b4015520ac2f2788aed72311afa8993e1c57
gary-burke <garylewisburke@gmail.com>
2025-12-23 17:21:36 +0100

    Add Modal to confirm book delete

--- static/js/library.js
+++ static/js/library.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+    const deleteModal = new bootstrap.Modal($("#deleteModal"));
+    const deleteConfirm = $("#deleteConfirm");
+
     $(".card").hover(function () {
         $(this).find("a").toggleClass("hide");
     });
 
     $(".button-delete").on("click", function (e) {
         let bookId = $(this).attr("book_id");
-        $(this).attr("href", `delete_book/${bookId}`);
+        deleteConfirm.attr("href", `delete_book/${bookId}`);
+        deleteModal.show();
     });
 });
