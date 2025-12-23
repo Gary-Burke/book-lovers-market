b6972c954285309ea63c4ecddb75aba12c79a3a1
gary-burke <garylewisburke@gmail.com>
2025-12-23 20:02:34 +0100

    Add form to edit books

--- library/forms.py
+++ library/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Book
 import re
 
 
@@ -12,3 +13,18 @@ class ISBNForm(forms.Form):
     def clean_isbn(self):
         isbn = self.cleaned_data["isbn"]
         return re.sub(r"[-\s]", "", isbn)
+
+
+class EditBookForm(forms.Form):
+    """
+    Form class for users to edit their book details
+    """
+    class Meta:
+        """
+        Django model: Book
+        Fields to add to form model
+        """
+        model = Book
+        fields = (
+            "title", "author", "isbn", "cover_url", "status", "price"
+        )
