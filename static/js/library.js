ae4754ef755af8af4d99307fa484f4c06e4d189e
gary-burke <garylewisburke@gmail.com>
2025-12-23 17:38:17 +0100

    Change edit button to link element and add comments to JS functions

--- static/js/library.js
+++ static/js/library.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
     const deleteModal = new bootstrap.Modal($("#deleteModal"));
-    const deleteConfirm = $("#deleteConfirm");
 
+    /**
+     * Shows/Hides edit and delete icons when individual cards are hovered on
+     */
     $(".card").hover(function () {
         $(this).find("a").toggleClass("hide");
     });
 
-    $(".button-delete").on("click", function (e) {
+    /**
+     * When clicked, gets book ID from delete button attribute
+     * Builds dynamic href/URL for selected book
+     * Triggers bootstrap delete modal confirmation before deleting book
+     */
+    $(".button-delete").on("click", function () {
         let bookId = $(this).attr("book_id");
-        deleteConfirm.attr("href", `delete_book/${bookId}`);
+        $("#deleteConfirm").attr("href", `delete_book/${bookId}`);
         deleteModal.show();
     });
 });
