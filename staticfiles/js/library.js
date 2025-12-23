b9e346c0309f642522ddcd1c32a926b6cff6ace6
gary-burke <garylewisburke@gmail.com>
2025-12-23 23:03:22 +0100

    Run collectstatic for deployment

---
+++ staticfiles/js/library.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    const deleteModal = new bootstrap.Modal($("#deleteModal"));
+
+    /**
+     * Shows/Hides edit and delete icons when individual cards are hovered on
+     */
+    $(".card").hover(function () {
+        $(this).find("a").toggleClass("hide");
+    });
+
+    /**
+     * When clicked, gets book ID from delete button attribute
+     * Builds dynamic href/URL for selected book
+     * Triggers bootstrap delete modal confirmation before deleting book
+     */
+    $(".button-delete").on("click", function () {
+        let bookId = $(this).attr("book_id");
+        $("#deleteConfirm").attr("href", `delete_book/${bookId}`);
+        deleteModal.show();
+    });
+
+    $(".button-edit").on("click", function () {
+        let bookId = $(this).attr("book_id");
+        $(".button-edit").attr("href", `edit_book/${bookId}`);
+        $("#editBookForm").attr("action", `edit_book/${bookId}`);
+    });
+});
