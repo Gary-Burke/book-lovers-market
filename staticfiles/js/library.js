a079f11fcaba3e52cda79c9c2a2755f22d42d2b6
gary-burke <garylewisburke@gmail.com>
2025-12-26 16:23:22 +0100

    Run collectstatic for deployment

--- staticfiles/js/library.js
+++ staticfiles/js/library.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     /**
      * Shows/Hides edit and delete icons when individual cards are hovered on
      */
-    $(".card").hover(function () {
+    $(".book").hover(function () {
         $(this).find("a").toggleClass("hide");
     });
 
@@ -19,9 +19,29 @@ $(document).ready(function () {
         deleteModal.show();
     });
 
+    /**
+     * When clicked, gets book ID from edit button attribute
+     * Builds dynamic href/URL for selected book
+     * builds url for action attribute of post form
+     */
     $(".button-edit").on("click", function () {
         let bookId = $(this).attr("book_id");
         $(".button-edit").attr("href", `edit_book/${bookId}`);
         $("#editBookForm").attr("action", `edit_book/${bookId}`);
     });
+
+    /**
+     * Toggles dropdown menu when filter icon is clicked
+     */
+    $("#library-filter").on("click", function () {
+        $("#form-filter").toggleClass("hide");
+    });
+
+    /**
+     * Toggles dropdown menu when sort by icon is clicked
+     */
+    $("#library-sort").on("click", function () {
+        $("#form-sort").toggleClass("hide");
+    });
+
 });
