2666b0d7726c1b75b011eec86c2485ed9cb8a529
gary-burke <garylewisburke@gmail.com>
2025-12-27 20:20:56 +0100

    Update url for sales page

--- library/urls.py
+++ library/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add/', views.add_book_by_isbn, name='add_book'),
     path('delete_book/<int:book_id>', views.delete_book, name='delete_book'),
     path('edit_book/<int:book_id>', views.edit_book, name='edit_book'),
-    path('sales/', views.sales_page, name='sales'),
+    path('sales/', views.SalesList.as_view(), name='sales'),
 ]
