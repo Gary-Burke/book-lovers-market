971f4653f7a43810b84e037e240ee49a02742ba6
gary-burke <garylewisburke@gmail.com>
2025-12-27 17:21:21 +0100

    Add url for sales page

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('about/', views.about_us, name='about'),
+    path('sales/', views.sales_page, name='sales'),
     path('', views.home_page, name='home'),
 ]
