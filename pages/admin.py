a3796185337cb584ddb95850416f4397f45713a1
gary-burke <garylewisburke@gmail.com>
2025-12-28 20:20:16 +0100

    Register Home model in admin page

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Home
+
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    """
+    Lists message and read fields for display in admin
+    """
+    list_display = ("quote", "author", "created_on")
+    search_fields = ["quote", "author"]
+
+    class Meta:
+        ordering = ["-created_on"]
 
 # Register your models here.
