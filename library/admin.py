97b8911b4eef0c40626c1a7786ab3d7e6631220f
gary-burke <garylewisburke@gmail.com>
2025-12-28 19:04:17 +0100

    Update admin fields for Book class

--- library/admin.py
+++ library/admin.py
@@ -8,9 +8,8 @@ class BookAdmin(admin.ModelAdmin):
     Lists message and read fields for display in admin
     """
     list_display = ("title", "author", "isbn", "status")
-    search_fields = ["title", "author", "isbn", "status"]
-    list_filter = ("title", "author", "isbn", "status")
-    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ["title", "author", "isbn"]
+    list_filter = ("status",)
 
 
 # Register your models here.
