92b4746c11f6fd30f3526cf05fb70bfef65f2361
gary-burke <garylewisburke@gmail.com>
2025-12-28 19:39:17 +0100

    Add model Home to Pages app

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Home(models.Model):
+    image = CloudinaryField('image', default='placeholder')
+    quote = models.TextField()
+    author = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author | self.quote
