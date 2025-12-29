fb3da586608ace511634d7ee1983e6fc768fc39a
gary-burke <garylewisburke@gmail.com>
2025-12-29 09:51:26 +0100

    Add form for Feedback model

---
+++ pages/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Feedback
+
+
+class FeedbackForm(forms.ModelForm):
+    """
+    Form class for users to provide feedback
+    """
+    class Meta:
+        """
+        Django model: Feedback
+        Fields to add to form
+        """
+        model = Feedback
+        fields = (
+            "name", "email", "body"
+        )
