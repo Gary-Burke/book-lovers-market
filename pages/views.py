91e34ff6af4f866ae60a2b5501533f3595aff62c
gary-burke <garylewisburke@gmail.com>
2025-12-29 11:28:33 +0100

    Add code views to handle Post request from feedback form

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import Home
+from .forms import FeedbackForm
 
 # Create your views here.
 
 
 def about_us(request):
+
+    if request.method == "POST":
+        feedback_form = FeedbackForm(data=request.POST)
+        if feedback_form.is_valid():
+            feedback_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Feedback submitted! Thank you for your input"
+            )
+        else:
+            messages.add_message(
+                request, messages.ERROR,
+                "Unable to submit feedback. Please try again later"
+            )
+
+    feedback_form = FeedbackForm()
+
     return render(
         request,
         "pages/about.html",
+        {
+            "feedback_form": feedback_form,
+        }
     )
 
 
