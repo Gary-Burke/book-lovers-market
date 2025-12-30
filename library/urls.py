d2de86db82892fad4241a194691f3c9cdde13bd5
gary-burke <garylewisburke@gmail.com>
2025-12-30 20:58:17 +0100

    Add url for view to add books manually

--- library/urls.py
+++ library/urls.py
@@ -6,6 +6,7 @@ from .views import BookDetailView
 urlpatterns = [
     path('', views.BookList.as_view(), name='library'),
     path('add/', views.add_book_by_isbn, name='add_book'),
+    path('add_manual/', views.add_book_manual, name='add_book_manual'),
     path('delete_book/<int:book_id>', views.delete_book, name='delete_book'),
     path('edit_book/<int:book_id>', views.edit_book, name='edit_book'),
     path('sales/', views.SalesList.as_view(), name='sales'),
