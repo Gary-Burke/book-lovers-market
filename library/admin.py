55bf9a109e74e644151c027b701a44e736e1afa9
gary-burke <garylewisburke@gmail.com>
2026-01-02 21:07:07 +0100

    Update BookAdmin class display fields

--- library/admin.py
+++ library/admin.py
@@ -5,9 +5,9 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     """
-    Lists message and read fields for display in admin
+    Lists the title, author, isbn, status and price fields for display in admin
     """
-    list_display = ("title", "author", "isbn", "status")
+    list_display = ("title", "author", "isbn", "status", "price")
     search_fields = ["title", "author", "isbn"]
     list_filter = ("status",)
 
