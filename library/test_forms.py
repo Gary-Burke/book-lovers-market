5386b2cd3c13cb43337cec02b5d6ff2ecdfb0b63
gary-burke <garylewisburke@gmail.com>
2026-01-03 10:43:32 +0100

    Add tests for EditBookForm

--- library/test_forms.py
+++ library/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import ISBNForm
+from .forms import ISBNForm, EditBookForm
 
 # Create your tests here.
 
@@ -14,3 +14,66 @@ class TestISBNForm(TestCase):
         self.assertFalse(
             form.is_valid(),
             msg="ISBN must be entered")
+
+
+class TestEditBookForm(TestCase):
+
+    def test_form_is_valid(self):
+        """Test all field to be valid"""
+        form = EditBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertTrue(
+            form.is_valid(),
+            msg="Form is valid")
+
+    def test_title_is_required(self):
+        """Test 'title' field to be not empty"""
+        form = EditBookForm({
+            "title": "",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Title was not entered but form is valid")
+    
+    def test_author_is_required(self):
+        """Test 'author' field to be not empty"""
+        form = EditBookForm({
+            "title": "Some Book",
+            "author": "",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Author was not entered but form is valid")
+        
+    def test_isbn_is_required(self):
+        """Test 'isbn' field to be not empty"""
+        form = EditBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="ISBN was not entered but form is valid")
+        
+    def test_status_is_required(self):
+        """Test 'status' field to be not empty"""
+        form = EditBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": ""
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Status was not entered but form is valid")
