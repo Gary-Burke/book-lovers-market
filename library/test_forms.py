5c830a520455736d521d18c3a341315372816e55
gary-burke <garylewisburke@gmail.com>
2026-01-03 10:46:01 +0100

    Add tests for ManualBookForm

--- library/test_forms.py
+++ library/test_forms.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .forms import ISBNForm, EditBookForm
+from .forms import ISBNForm, EditBookForm, ManualBookForm
 
 # Create your tests here.
 
@@ -41,7 +41,7 @@ class TestEditBookForm(TestCase):
         self.assertFalse(
             form.is_valid(),
             msg="Title was not entered but form is valid")
-    
+
     def test_author_is_required(self):
         """Test 'author' field to be not empty"""
         form = EditBookForm({
@@ -53,7 +53,7 @@ class TestEditBookForm(TestCase):
         self.assertFalse(
             form.is_valid(),
             msg="Author was not entered but form is valid")
-        
+
     def test_isbn_is_required(self):
         """Test 'isbn' field to be not empty"""
         form = EditBookForm({
@@ -65,10 +65,73 @@ class TestEditBookForm(TestCase):
         self.assertFalse(
             form.is_valid(),
             msg="ISBN was not entered but form is valid")
-        
+
     def test_status_is_required(self):
         """Test 'status' field to be not empty"""
         form = EditBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": ""
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Status was not entered but form is valid")
+
+
+class TestManualBookForm(TestCase):
+
+    def test_form_is_valid(self):
+        """Test all field to be valid"""
+        form = ManualBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertTrue(
+            form.is_valid(),
+            msg="Form is valid")
+
+    def test_title_is_required(self):
+        """Test 'title' field to be not empty"""
+        form = ManualBookForm({
+            "title": "",
+            "author": "Some Author",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Title was not entered but form is valid")
+
+    def test_author_is_required(self):
+        """Test 'author' field to be not empty"""
+        form = ManualBookForm({
+            "title": "Some Book",
+            "author": "",
+            "isbn": "123456789",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="Author was not entered but form is valid")
+
+    def test_isbn_is_required(self):
+        """Test 'isbn' field to be not empty"""
+        form = ManualBookForm({
+            "title": "Some Book",
+            "author": "Some Author",
+            "isbn": "",
+            "status": "0"
+        })
+        self.assertFalse(
+            form.is_valid(),
+            msg="ISBN was not entered but form is valid")
+
+    def test_status_is_required(self):
+        """Test 'status' field to be not empty"""
+        form = ManualBookForm({
             "title": "Some Book",
             "author": "Some Author",
             "isbn": "123456789",
