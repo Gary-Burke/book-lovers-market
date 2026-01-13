0f79f3bd9ccb6428a13e84f8267fe777d1de6d06
gary-burke <garylewisburke@gmail.com>
2026-01-13 20:45:52 +0100

    Add user to BookAdmin class fields

--- library/admin.py
+++ library/admin.py
@@ -5,10 +5,11 @@ from .models import Book
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     """
-    Lists the title, author, isbn, status and price fields for display in admin
+    Lists the title, author, isbn, status, price and user fields
+    for display in admin
     """
-    list_display = ("title", "author", "isbn", "status", "price")
-    search_fields = ["title", "author", "isbn"]
+    list_display = ("title", "author", "isbn", "status", "price", "user")
+    search_fields = ["title", "author", "isbn", "user"]
     list_filter = ("status",)
 
 
