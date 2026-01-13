481b0d403f54eb84dd68a393062ce58d031e91a1
gary-burke <garylewisburke@gmail.com>
2026-01-13 20:39:35 +0100

    Move and declare const detailModal in Global scope

--- static/js/sales.js
+++ static/js/sales.js
@@ -3,12 +3,10 @@
 // Global Variables
 let timer;
 const delay = 500;
-let detailModal;
+const detailModal = new bootstrap.Modal(document.getElementById("detailModal"));
 
 // Wait for the DOM to load before executing functions
 document.addEventListener("DOMContentLoaded", () => {
-
-    detailModal = new bootstrap.Modal(document.getElementById("detailModal"));
 
     let books = document.querySelectorAll(".book");
 
@@ -29,15 +27,15 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /**
-     * Loads Bootstrap modal with book details when clicked via partial template
-     */
+ * Loads Bootstrap modal with book details when clicked via partial template
+ */
 async function getURL(e) {
     try {
         const url = e.currentTarget.getAttribute("data-url");
         const response = await fetch(url);
 
         if (!response.ok) {
-            throw new Error(`Server Status: ${response.status}`)
+            throw new Error(`Server Status: ${response.status}`);
         }
 
         document.getElementById("detailModalBody").innerHTML = await response.text();
