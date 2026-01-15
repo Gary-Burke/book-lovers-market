2c19b20eb843041d319710ba210ac049712aeb84
gary-burke <garylewisburke@gmail.com>
2026-01-15 10:55:41 +0100

    Add preventDefault to form selection on Add Book page

--- static/js/add.js
+++ static/js/add.js
@@ -6,8 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
         a.addEventListener("click", function (e) {
             var url = this.getAttribute("data-url");
 
+            // Loads form based on user selected button
             if (!this.classList.contains("selected")) {
                 this.setAttribute("href", url);
+            } else {
+                e.preventDefault();
             }
         });
     });
